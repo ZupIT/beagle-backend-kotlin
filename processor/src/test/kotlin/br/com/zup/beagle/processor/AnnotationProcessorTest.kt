4fa0aa3f670729e1cc4795bde0f38d78efa96657
Yan <yan.dias@zup.com.br>
2021-08-27 09:31:18 -0300

    Refac Tests

--- processor/src/test/kotlin/br/com/zup/beagle/processor/AnnotationProcessorTest.kt
+++ processor/src/test/kotlin/br/com/zup/beagle/processor/AnnotationProcessorTest.kt
@@ -16,57 +16,20 @@
 
 package br.com.zup.beagle.processor
 
-import br.com.zup.beagle.annotation.ContextObject
 import br.com.zup.beagle.widget.action.SetContext
 import br.com.zup.beagle.widget.context.Bind
-import br.com.zup.beagle.widget.context.Context
 import br.com.zup.beagle.widget.context.expressionOf
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@ContextObject
-data class Person(
-    override val id: String = "",
-    val name: String,
-    val age: Int,
-    val orders: List<Order>,
-    val address: Address
-): Context {
-    constructor(id: String): this(id, "", 12, listOf(), Address(""))
-}
-
-@ContextObject
-data class Order(
-    override val id: String = "",
-    val products: List<String>,
-    val value: Double
-): Context {
-    constructor(id: String): this(id, listOf(), 0.0)
-}
-
-@ContextObject
-data class Address(
-    override val id: String = "",
-    val street: String,
-    val zupCode: String,
-    val contact: Contact
-): Context {
-    constructor(id: String): this(id, "", "", Contact(""))
-}
-
-@ContextObject
-data class Contact(
-    override val id: String = "",
-    val email: String,
-    val number: String
-): Context {
-    constructor(id: String): this(id, "", "")
-}
-
+@DisplayName("Given a context annotated with ContextObject")
 internal class AnnotationProcessorTest {
 
     companion object {
         const val contextId = "contextId"
+        const val globalId = "global"
 
         private val person = Person(
             name = "yan",
@@ -86,129 +49,200 @@ internal class AnnotationProcessorTest {
                 )
             )
         ).normalize(contextId)
+
+        private val global = Global(
+            name = "yan",
+            age = 23,
+            contact = Contact(
+                email = "yan@mail.com",
+                number = "00000000"
+            )
+        ).normalize()
     }
 
-    @Test
-    fun test_generated_normalize() {
-        val expectedAddressContextId = "$contextId.${Person::address.name}"
-        val expectedContactContextId = "$contextId.${Person::address.name}.${Address::contact.name}"
-        val expectedOrderContextId = "$contextId.${Person::orders.name}[0]"
+    @DisplayName("When use normalize")
+    @Nested
+    inner class NormalizerTest {
 
-        Assertions.assertEquals(contextId, person.id)
-        Assertions.assertEquals(expectedAddressContextId, person.address.id)
-        Assertions.assertEquals(expectedContactContextId, person.address.contact.id)
-        Assertions.assertEquals(expectedOrderContextId, person.orders[0].id)
+        @DisplayName("Then should updated context ids")
+        @Test
+        fun test_generated_normalize() {
+            val expectedAddressContextId = "$contextId.${Person::address.name}"
+            val expectedContactContextId = "$contextId.${Person::address.name}.${Address::contact.name}"
+            val expectedOrderContextId = "$contextId.${Person::orders.name}[0]"
+
+            Assertions.assertEquals(contextId, person.id)
+            Assertions.assertEquals(expectedAddressContextId, person.address.id)
+            Assertions.assertEquals(expectedContactContextId, person.address.contact.id)
+            Assertions.assertEquals(expectedOrderContextId, person.orders[0].id)
+        }
     }
 
-    @Test
-    fun test_generated_root_expression() {
-        val expectedPersonExpression: Bind.Expression<Int> = expressionOf("@{${person.id}}")
-        val expectedAddressExpression: Bind.Expression<String> = expressionOf("@{${person.address.id}}")
-        val expectedContactExpression: Bind.Expression<String> = expressionOf("@{${person.address.contact.id}}")
-        val expectedOrdersExpression: Bind.Expression<List<String>> = expressionOf("@{$contextId.${Person::orders.name}}")
-        val expectedOrderElementExpression: Bind.Expression<Order> = expressionOf("@{${person.orders[0].id}}")
+    @DisplayName("When use expressions")
+    @Nested
+    inner class ExpressionsTests {
 
-        Assertions.assertEquals(expectedPersonExpression, person.expression)
-        Assertions.assertEquals(expectedAddressExpression, person.address.expression)
-        Assertions.assertEquals(expectedAddressExpression, person.addressExpression)
-        Assertions.assertEquals(expectedContactExpression, person.address.contact.expression)
-        Assertions.assertEquals(expectedContactExpression, person.address.contactExpression)
-        Assertions.assertEquals(expectedOrdersExpression, person.ordersExpression)
-        Assertions.assertEquals(expectedOrderElementExpression, person.orders[0].expression)
+        @DisplayName("Then should return correct expressions with correct types")
+        @Test
+        fun test_generated_root_expression() {
+            val expectedPersonExpression: Bind.Expression<Int> = expressionOf("@{${person.id}}")
+            val expectedAddressExpression: Bind.Expression<String> = expressionOf("@{${person.address.id}}")
+            val expectedContactExpression: Bind.Expression<String> = expressionOf("@{${person.address.contact.id}}")
+            val expectedOrdersExpression: Bind.Expression<List<String>> = expressionOf("@{$contextId.${Person::orders.name}}")
+            val expectedOrderElementExpression: Bind.Expression<Order> = expressionOf("@{${person.orders[0].id}}")
+
+            Assertions.assertEquals(expectedPersonExpression, person.expression)
+            Assertions.assertEquals(expectedAddressExpression, person.address.expression)
+            Assertions.assertEquals(expectedAddressExpression, person.addressExpression)
+            Assertions.assertEquals(expectedContactExpression, person.address.contact.expression)
+            Assertions.assertEquals(expectedContactExpression, person.address.contactExpression)
+            Assertions.assertEquals(expectedOrdersExpression, person.ordersExpression)
+            Assertions.assertEquals(expectedOrderElementExpression, person.orders[0].expression)
+        }
+
+        @DisplayName("Then should return correct expressions with correct types")
+        @Test
+        fun test_generated_expressions() {
+            val expectedAgeExpression: Bind.Expression<Int> = expressionOf("@{${person.id}.${Person::age.name}}")
+            val expectedStreetExpression: Bind.Expression<String> = expressionOf("@{${person.address.id}.${Address::street.name}}")
+            val expectedEmailExpression: Bind.Expression<String> = expressionOf("@{${person.address.contact.id}.${Contact::email.name}}")
+            val expectedProductsExpression: Bind.Expression<List<String>> = expressionOf("@{${person.orders[0].id}.${Order::products.name}}")
+
+            Assertions.assertEquals(expectedAgeExpression, person.ageExpression)
+            Assertions.assertEquals(expectedStreetExpression, person.address.streetExpression)
+            Assertions.assertEquals(expectedEmailExpression, person.address.contact.emailExpression)
+            Assertions.assertEquals(expectedProductsExpression, person.orders[0].productsExpression)
+        }
     }
 
-    @Test
-    fun test_generated_expressions() {
-        val expectedAgeExpression: Bind.Expression<Int> = expressionOf("@{${person.id}.${Person::age.name}}")
-        val expectedStreetExpression: Bind.Expression<String> = expressionOf("@{${person.address.id}.${Address::street.name}}")
-        val expectedEmailExpression: Bind.Expression<String> = expressionOf("@{${person.address.contact.id}.${Contact::email.name}}")
-        val expectedProductsExpression: Bind.Expression<List<String>> = expressionOf("@{${person.orders[0].id}.${Order::products.name}}")
+    @DisplayName("When use change")
+    @Nested
+    inner class ChangeTests {
 
-        Assertions.assertEquals(expectedAgeExpression, person.ageExpression)
-        Assertions.assertEquals(expectedStreetExpression, person.address.streetExpression)
-        Assertions.assertEquals(expectedEmailExpression, person.address.contact.emailExpression)
-        Assertions.assertEquals(expectedProductsExpression, person.orders[0].productsExpression)
+        @DisplayName("Then should return a SetContext action with correct attributes")
+        @Test
+        fun test_generated_root_change_functions() {
+            val newAddress = Address(id = contextId)
+            val newPerson = Person(id = contextId)
+            val newContact = Contact(id = contextId)
+            val newOrder = Order(id = contextId)
+
+            assertChange(newPerson, null, person.change(newPerson))
+            assertChange(newAddress, Person::address.name, person.address.change(newAddress))
+            assertChange(newContact, "${Person::address.name}.${Address::contact.name}", person.address.contact.change(newContact))
+            assertChange(newOrder, Person::orders.name + "[0]", person.orders[0].change(newOrder))
+        }
+
+        @DisplayName("Then should return a SetContext action with correct attributes")
+        @Test
+        fun test_generated_root_change_functions_bind() {
+            val newAddressBind = expressionOf<Address>("@{context.address}")
+            val newPersonBind = expressionOf<Person>("@{context.person}")
+            val newContactBind = expressionOf<Contact>("@{context.contact}")
+            val newOrderBind = expressionOf<Order>("@{context.order}")
+
+            assertChange(newPersonBind, null, person.change(newPersonBind))
+            assertChange(newAddressBind, Person::address.name, person.address.change(newAddressBind))
+            assertChange(newContactBind, "${Person::address.name}.${Address::contact.name}", person.address.contact.change(newContactBind))
+            assertChange(newOrderBind, Person::orders.name + "[0]", person.orders[0].change(newOrderBind))
+        }
+
+        @DisplayName("Then should return a SetContext action with correct attributes")
+        @Test
+        fun test_generated_change_functions() {
+            val newName = "pocas"
+            val newStreet = "Av Joao Naves"
+            val newEmail = "pocas@mail.com"
+            val newValue = 15.000
+
+            assertChange(newName, Person::name.name, person.changeName(newName))
+            assertChange(newStreet, "${Person::address.name}.${Address::street.name}", person.address.changeStreet(newStreet))
+            assertChange(newEmail, "${Person::address.name}.${Address::contact.name}.${Contact::email.name}", person.address.contact.changeEmail(newEmail))
+            assertChange(newValue, "${Person::orders.name}[0].${Order::value.name}", person.orders[0].changeValue(newValue))
+        }
+
+        @DisplayName("Then should return a SetContext action with correct attributes")
+        @Test
+        fun test_generated_change_functions_bind() {
+            val newName = expressionOf<String>("context.name")
+            val newStreet = expressionOf<String>("context.street")
+            val newEmail = expressionOf<String>("context.email")
+            val newValue = expressionOf<Double>("context.value")
+
+            assertChange(newName, Person::name.name, person.changeName(newName))
+            assertChange(newStreet, "${Person::address.name}.${Address::street.name}", person.address.changeStreet(newStreet))
+            assertChange(newEmail, "${Person::address.name}.${Address::contact.name}.${Contact::email.name}", person.address.contact.changeEmail(newEmail))
+            assertChange(newValue, "${Person::orders.name}[0].${Order::value.name}", person.orders[0].changeValue(newValue))
+        }
     }
 
-    @Test
-    fun test_generated_root_change_functions() {
-        val newAddress = Address(id = contextId)
-        val newPerson = Person(id = contextId)
-        val newContact = Contact(id = contextId)
-        val newOrder = Order(id = contextId)
+    @DisplayName("When use list access functions")
+    @Nested
+    inner class ListAccessTest {
 
-        assertChange(newPerson, null, person.change(newPerson))
-        assertChange(newAddress, Person::address.name, person.address.change(newAddress))
-        assertChange(newContact, "${Person::address.name}.${Address::contact.name}", person.address.contact.change(newContact))
-        assertChange(newOrder, Person::orders.name + "[0]", person.orders[0].change(newOrder))
+        @DisplayName("Then should return a element with correct contextId")
+        @Test
+        fun test_generated_get_element_functions() {
+            val index = 40
+            val expectedOrderContextId = "contextId.orders[$index]"
+
+            Assertions.assertEquals(expectedOrderContextId, person.ordersGetElementAt(index).id)
+            Assertions.assertEquals(person.orders[0], person.ordersGetElementAt(0), "When element exist it must return it")
+        }
+
+        @DisplayName("Then should return a SetContext with correct attributes")
+        @Test
+        fun test_generated_change_element_functions() {
+            val index = 30
+            val newProduct = "newProduct"
+            val newOrder = Order(id = "contextId", products = listOf("newProducts"), value = 30.00)
+
+            assertChange(newProduct, "${Person::orders.name}[0].${Order::products.name}[$index]", person.orders[0].changeProductsElement(newProduct, index))
+            assertChange(newOrder, "${Person::orders.name}[$index]", person.changeOrdersElement(newOrder, index))
+        }
+
+        @DisplayName("Then should return a SetContext with correct attributes")
+        @Test
+        fun test_generated_change_element_functions_bind() {
+            val index = 30
+            val newProduct = expressionOf<String>("context.product")
+            val newOrder = expressionOf<Order>("context.order")
+
+            assertChange(newProduct, "${Person::orders.name}[0].${Order::products.name}[$index]", person.orders[0].changeProductsElement(newProduct, index))
+            assertChange(newOrder, "${Person::orders.name}[$index]", person.changeOrdersElement(newOrder, index))
+        }
     }
 
-    @Test
-    fun test_generated_root_change_functions_bind() {
-        val newAddressBind = expressionOf<Address>("@{context.address}")
-        val newPersonBind = expressionOf<Person>("@{context.person}")
-        val newContactBind = expressionOf<Contact>("@{context.contact}")
-        val newOrderBind = expressionOf<Order>("@{context.order}")
+    @DisplayName("When use Global Context generated functions")
+    @Nested
+    inner class GlobalContextTest {
 
-        assertChange(newPersonBind, null, person.change(newPersonBind))
-        assertChange(newAddressBind, Person::address.name, person.address.change(newAddressBind))
-        assertChange(newContactBind, "${Person::address.name}.${Address::contact.name}", person.address.contact.change(newContactBind))
-        assertChange(newOrderBind, Person::orders.name + "[0]", person.orders[0].change(newOrderBind))
-    }
+        @DisplayName("Then should update context ids")
+        @Test
+        fun test_normalize() {
+            val expectedContactContextId = "global.${Global::contact.name}"
 
-    @Test
-    fun test_generated_change_functions() {
-        val newName = "pocas"
-        val newStreet = "Av Joao Naves"
-        val newEmail = "pocas@mail.com"
-        val newValue = 15.000
+            Assertions.assertEquals(expectedContactContextId, global.contact.id)
+        }
 
-        assertChange(newName, Person::name.name, person.changeName(newName))
-        assertChange(newStreet, "${Person::address.name}.${Address::street.name}", person.address.changeStreet(newStreet))
-        assertChange(newEmail, "${Person::address.name}.${Address::contact.name}.${Contact::email.name}", person.address.contact.changeEmail(newEmail))
-        assertChange(newValue, "${Person::orders.name}[0].${Order::value.name}", person.orders[0].changeValue(newValue))
-    }
+        @DisplayName("Then should return correct expressions")
+        @Test
+        fun test_expression_properties() {
+            val expectedGlobalExpression: Bind.Expression<Global> = expressionOf("@{global}")
+            val expectedNameExpression: Bind.Expression<String> = expressionOf("@{global.${Global::name.name}}")
 
-    @Test
-    fun test_generated_change_functions_bind() {
-        val newName = expressionOf<String>("context.name")
-        val newStreet = expressionOf<String>("context.street")
-        val newEmail = expressionOf<String>("context.email")
-        val newValue = expressionOf<Double>("context.value")
+            Assertions.assertEquals(expectedGlobalExpression, global.expression)
+            Assertions.assertEquals(expectedNameExpression, global.nameExpression)
+        }
 
-        assertChange(newName, Person::name.name, person.changeName(newName))
-        assertChange(newStreet, "${Person::address.name}.${Address::street.name}", person.address.changeStreet(newStreet))
-        assertChange(newEmail, "${Person::address.name}.${Address::contact.name}.${Contact::email.name}", person.address.contact.changeEmail(newEmail))
-        assertChange(newValue, "${Person::orders.name}[0].${Order::value.name}", person.orders[0].changeValue(newValue))
-    }
+        @DisplayName("Then should return correct SetContexts")
+        @Test
+        fun text_change_functions() {
+            val newName = expressionOf<String>("context.name")
+            val expectedSetContext = SetContext(contextId = "global", value = newName, path = "name")
 
-    @Test
-    fun test_generated_get_element_functions() {
-        val index = 40
-        val expectedOrderContextId = "contextId.orders[$index]"
-
-        Assertions.assertEquals(expectedOrderContextId, person.ordersGetElementAt(index).id)
-        Assertions.assertEquals(person.orders[0], person.ordersGetElementAt(0), "When element exist it must return it")
-    }
-
-    @Test
-    fun test_generated_change_element_functions() {
-        val index = 30
-        val newProduct = "newProduct"
-        val newOrder = Order(id = "contextId", products = listOf("newProducts"), value = 30.00)
-
-        assertChange(newProduct, "${Person::orders.name}[0].${Order::products.name}[$index]", person.orders[0].changeProductsElement(newProduct, index))
-        assertChange(newOrder, "${Person::orders.name}[$index]", person.changeOrdersElement(newOrder, index))
-    }
-
-    @Test
-    fun test_generated_change_element_functions_bind() {
-        val index = 30
-        val newProduct = expressionOf<String>("context.product")
-        val newOrder = expressionOf<Order>("context.order")
-
-        assertChange(newProduct, "${Person::orders.name}[0].${Order::products.name}[$index]", person.orders[0].changeProductsElement(newProduct, index))
-        assertChange(newOrder, "${Person::orders.name}[$index]", person.changeOrdersElement(newOrder, index))
+            Assertions.assertEquals(expectedSetContext, global.changeName(newName))
+        }
     }
 
     private fun assertChange(value: Any, path: String?, setContext: SetContext) {
